800ba563167ab1f33f553adc951c9bdce08b5dcb
zeonga1102 <dlwjddk4587@naver.com>
2022-09-11 06:23:43 +0900

    STY: url 변경

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup', views.UserView.as_view(), name='signup'),
-    path('login-out', views.UserApiView.as_view(), name='login_out'),
+    path('login', views.UserApiView.as_view(), name='login'),
     path('', views.IndexView.as_view(), name='index')
 ]
