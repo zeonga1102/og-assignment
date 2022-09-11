a9494e5cc139c39700088ea7345f5fad7b612b51
zeonga1102 <dlwjddk4587@naver.com>
2022-09-11 20:41:01 +0900

    ENH: 작가 정보, 작품 정보 조회 구현

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.UserView.as_view(), name='signup'),
-    path('login', views.UserApiView.as_view(), name='login'),
-    path('', views.IndexView.as_view(), name='index')
+    path("signup", views.UserView.as_view(), name="signup"),
+    path("login", views.UserApiView.as_view(), name="login"),
+    path("", views.IndexView.as_view(), name="index"),
+
+    path("info/<str:type>", views.InfoView.as_view(), name="info"),
 ]
--- user/views.py
+++ user/views.py
@@ -4,12 +4,18 @@ from django.contrib.auth import login, logout, authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from artist.models import Artist
+from artist.models import Work
+
 from .serializers import UserSerializer
+from artist.serializers import ArtistSerializer
+from artist.serializers import WorkSerializer
 
 
 class UserView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'sign_up.html'
+    template_name = "sign_up.html"
 
     def get(self, request):
         return Response({"message": "정상"}, status=status.HTTP_200_OK)
@@ -20,35 +26,35 @@ class UserView(APIView):
 
         if user_serializer.is_valid():
             user_serializer.save()
-            return redirect('login')
+            return redirect("login")
             
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
-        return Response({'message': 'put method'})
+        return Response({"message": "put method"})
     
     def delete(self, request):
-        return Response({'message': 'delete method'})
+        return Response({"message": "delete method"})
 
 
 class UserApiView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'sign_in.html'
+    template_name = "sign_in.html"
 
     def get(self, request):
         return Response({"message": "정상"}, status=status.HTTP_200_OK)
 
     # 로그인
     def post(self, request):
-        username = request.data.get('username', '')
-        password = request.data.get('password', '')
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
 
         user = authenticate(request, username=username, password=password)
         if not user:
             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
         login(request, user)
-        return redirect('index')
+        return redirect("index")
 
     # 로그아웃
     def delete(self, request):
@@ -58,7 +64,24 @@ class UserApiView(APIView):
 
 class IndexView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'index.html'
+    template_name = "index.html"
 
     def get(self, request):
         return Response({"message": "정상"}, status=status.HTTP_200_OK)
+
+
+class InfoView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "info.html"
+
+    def get(self, request, type):
+        if type == "artist":
+            artist_data = Artist.objects.all()
+            serialized_artist_data = ArtistSerializer(artist_data, many=True).data
+            return Response({"type": "artist", "artists": serialized_artist_data}, status=status.HTTP_200_OK)
+        elif type == "work":
+            work_data = Work.objects.all()
+            serialized_work_data = WorkSerializer(work_data, many=True).data
+            return Response({"type": "work", "works": serialized_work_data}, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
