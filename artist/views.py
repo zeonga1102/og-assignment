e2d41602003e3f0a7038bf20374534174ea33fb2
zeonga1102 <dlwjddk4587@naver.com>
2022-09-12 01:45:45 +0900

    ENH: 작가 등록 페이지로 연결

--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework import status
 
-# Create your views here.
+class RegisterArtistView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "artist/register_artist.html"
+
+    def get(self, request):
+        return Response(status=status.HTTP_200_OK)
