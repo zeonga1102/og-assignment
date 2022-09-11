de4fed659fbb34848c29623cbbed0bebbe6ffb4f
zeonga1102 <dlwjddk4587@naver.com>
2022-09-12 05:11:10 +0900

    ENH: 작가 등록 신청 구현

--- artist/serializers.py
+++ artist/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
+
+import re
+
 from .models import Artist
 from .models import Work
 
 
 class ArtistSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        phone_re = re.compile("^010-\d{4}-\d{4}$")
+        if not phone_re.match(data.get("phone", None)):
+            raise serializers.ValidationError(
+                   detail={"error": "형식에 맞게 입력해주세요!"},
+               )
+
+        return data
+
     class Meta:
         model = Artist
         fields = ["user", "name", "gender", "birthday", "email", "phone", "signup_date", "status"]
--- artist/views.py
+++ artist/views.py
@@ -4,9 +4,24 @@ from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import status
 
+from .serializers import ArtistSerializer
+
 class RegisterArtistView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "artist/register_artist.html"
 
     def get(self, request):
         return Response(status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data.copy()
+
+        data["user"] = request.user.id
+        data["status"] = 2
+
+        artist_serializer = ArtistSerializer(data=data)
+        if artist_serializer.is_valid():
+            artist_serializer.save()
+            return redirect("index")
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
