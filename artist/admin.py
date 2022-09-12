b1d436dc2f4c1d720263518f281e3d2357f5bb68
zeonga1102 <dlwjddk4587@naver.com>
2022-09-12 22:59:03 +0900

    ENH: admin 페이지에 Exhibition Model 등록

--- artist/admin.py
+++ artist/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Status
 from .models import Artist
 from .models import Work
+from .models import Exhibition
 
 
 admin.site.register(Status)
 admin.site.register(Artist)
 admin.site.register(Work)
+admin.site.register(Exhibition)
