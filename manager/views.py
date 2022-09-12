f41aefa4791df8bc5baa5986d231b1681893fcde
zeonga1102 <dlwjddk4587@naver.com>
2022-09-13 00:45:57 +0900

    ENH: 관리자 대시보드 페이지 연결

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework import status
 
-# Create your views here.
+
+class DashboardView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "manager/dashboard.html"
+
+    def get(self, request):
+        return Response({"is_dashboard": True}, status=status.HTTP_200_OK)
--- open_gallery/urls.py
+++ open_gallery/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('user.urls')),
-    path('artist/', include('artist.urls')),
+    path("", include("user.urls")),
+    path("artist/", include("artist.urls")),
+    path("manager/", include("manager.urls")),
 ]
